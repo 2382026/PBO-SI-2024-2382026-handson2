1f4497ad4509616f514a6cc6b44583b41a1e5e81
2382026 <2382026@unai.edu>
2024-09-10 10:17:35 +0700

    handson 2

---
+++ BlockVsExpressionVsStatment.java
@@ -0,0 +1,13 @@
+public class BlockVsExpressionVsStatment {
+
+    public static void main(String[] args) {
+        //Ekspresi -> 10 + 10
+        //Ekspresi a = 20;
+        //Statment -> int a = 10 + 10;
+        int a = 10 + 10;
+        System.out.println("Rose Valencia");
+        System.out.println("Rose Valencia");
+        System.out.println("Rose Valencia");
+        System.out.println("Rose Valencia");
+    }
+}
