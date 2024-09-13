64d91d57067b6800f94e1f568e2343d0361c65a6
2382026 <2382026@unai.edu>
2024-09-13 16:03:46 +0700

    handson 2

--- Soal1.java
+++ Soal1.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 public class Soal1 {
-    /**
+    /*
      * Buatlah sebuah program yang mengambil 2 input number dari user
      * Program tersebut akan print out nilai yang paling besar
      */
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Masukkan angka Pertama : ");
+        int a = scanner.nextInt();
+
+        System.out.println("Masukkan angka Kedua : ");
+        int b = scanner.nextInt();
+
+        if (a > b) {
+            System.out.println("Angka terbesar adalah : " + a);
+        } else if (b > a) {
+            System.out.println("Angka terbesar adalah : " + b);
+        } else {
+            System.out.println("Kedua angka yang anda masukan sama besar.");
+        }
 
     }
 }
